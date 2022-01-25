1d7214029c1822528f1c6c7b102f4e0f59e8c318
K-MHoon <azmria117@gmail.com>
2022-01-25 22:48:53 +0900

    Initial commit

---
+++ src/test/java/com/example/units/UnitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.units;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
