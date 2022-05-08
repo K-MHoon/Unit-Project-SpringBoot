bb3c3fc781ef1607a8f11a0a12c03169a5808d2a
K-MHOON <azmria117@gmail.com>
2022-05-08 22:45:39 +0900

    feat[Ex4] : list Controller, HTML 추가

---
+++ ex04/src/main/java/com/example/ex04/controller/GuestBookController.java
@@ -0,0 +1,18 @@
+package com.example.ex04.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/guestbook")
+@Slf4j
+public class GuestBookController {
+
+    @GetMapping({"/", "/list"})
+    public String list() {
+        log.info("list");
+        return "/guestbook/list";
+    }
+}
