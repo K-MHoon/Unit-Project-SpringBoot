dee74729f330c0080e7ddde3186e9c5f2d5f887d
K-MHOON <azmria117@gmail.com>
2022-05-12 21:46:32 +0900

    feat[Ex4] : 컨트롤러 리스트 호출 및 HTML 리스트 내역 출력 구현

--- ex04/src/main/java/com/example/ex04/controller/GuestBookController.java
+++ ex04/src/main/java/com/example/ex04/controller/GuestBookController.java
@@ -1,18 +1,34 @@
 package com.example.ex04.controller;
 
+import com.example.ex04.dto.PageRequestDTO;
+import com.example.ex04.service.GuestBookService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/guestbook")
 @Slf4j
+@RequiredArgsConstructor
 public class GuestBookController {
 
-    @GetMapping({"/", "/list"})
-    public String list() {
-        log.info("list");
-        return "/guestbook/list";
+    private final GuestBookService service;
+
+    @GetMapping("/")
+    public String index() {
+        return "redirect:/guestbook/list";
     }
+
+    @GetMapping("/list")
+    public void list(PageRequestDTO pageRequestDTO, Model model) {
+        log.info("list...... " + pageRequestDTO);
+        model.addAttribute("result", service.getList(pageRequestDTO));
+    }
+
+
+
+
 }
