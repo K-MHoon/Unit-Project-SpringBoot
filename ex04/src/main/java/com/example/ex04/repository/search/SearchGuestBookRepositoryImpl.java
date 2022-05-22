7ee64f41e97a8f53aa468cbc1a41750f9443af03
K-MHOON <azmria117@gmail.com>
2022-05-22 19:02:19 +0900

    feat[Ex4] : QuerydslRepositorySupport 활용하여 데이터 조회

--- ex04/src/main/java/com/example/ex04/repository/search/SearchGuestBookRepositoryImpl.java
+++ ex04/src/main/java/com/example/ex04/repository/search/SearchGuestBookRepositoryImpl.java
@@ -1,8 +1,20 @@
 package com.example.ex04.repository.search;
 
 import com.example.ex04.entity.GuestBook;
+import com.example.ex04.entity.QGuestBook;
+import com.example.ex04.entity.QMember;
+import com.example.ex04.entity.QReply;
+import com.querydsl.core.Tuple;
+import com.querydsl.jpa.JPQLQuery;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+
+import java.util.List;
+
+import static com.example.ex04.entity.QGuestBook.guestBook;
+import static com.example.ex04.entity.QMember.member;
+import static com.example.ex04.entity.QReply.reply;
+
 
 @Slf4j
 public class SearchGuestBookRepositoryImpl extends QuerydslRepositorySupport implements SearchGuestBookRepository{
@@ -15,6 +27,19 @@ public class SearchGuestBookRepositoryImpl extends QuerydslRepositorySupport imp
     public GuestBook search1() {
 
         log.info("search1 ......");
+
+        JPQLQuery<GuestBook> jpqlQuery = from(guestBook);
+
+        jpqlQuery.leftJoin(member).on(guestBook.writer.eq(member));
+        jpqlQuery.leftJoin(reply).on(reply.guestBook.eq(guestBook));
+
+        JPQLQuery<Tuple> tuple = jpqlQuery.select(guestBook, member.email, reply.count()).groupBy(guestBook);
+
+        log.info("tuple = {}", tuple);
+
+        List<Tuple> result = tuple.fetch();
+
+        log.info("result = {}", result);
 
         return null;
     }
