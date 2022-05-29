ed235a581a9ae1407dfde81bc8018d8bf9bf137e
K-MHOON <azmria117@gmail.com>
2022-05-29 18:00:59 +0900

    feat[Ex5] : 리뷰 - 회원 삭제 추가, 테스트 (벌크 Delete)

--- ex05/src/main/java/com/example/ex05/repository/ReviewRepository.java
+++ ex05/src/main/java/com/example/ex05/repository/ReviewRepository.java
@@ -1,9 +1,12 @@
 package com.example.ex05.repository;
 
+import com.example.ex05.entity.member.Member;
 import com.example.ex05.entity.movie.Movie;
 import com.example.ex05.entity.review.Review;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -11,4 +14,8 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     @EntityGraph(attributePaths = {"member"}, type = EntityGraph.EntityGraphType.FETCH)
     List<Review> findByMovie(Movie movie);
+
+    @Modifying
+    @Query("delete from Review r where r.member = :member")
+    void deleteByMember(Member member);
 }
--- ex05/src/test/java/com/example/ex05/repository/MemberRepositoryTest.java
+++ ex05/src/test/java/com/example/ex05/repository/MemberRepositoryTest.java
@@ -1,22 +1,26 @@
 package com.example.ex05.repository;
 
 import com.example.ex05.entity.member.Member;
+import com.example.ex05.entity.review.Review;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.stream.IntStream;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class MemberRepositoryTest {
 
     @Autowired
     private MemberRepository memberRepository;
+
+    @Autowired
+    private ReviewRepository reviewRepository;
 
     @Test
     @Transactional
@@ -30,5 +34,20 @@ class MemberRepositoryTest {
                     .nickname("reviewer" + i).build();
             memberRepository.save(member);
         });
+    }
+
+    @Test
+    @DisplayName("회원 - 리뷰 삭제")
+    @Transactional
+    @Commit
+    public void testDeleteMember() {
+        Long mid = 69L;
+
+        Member member = Member.builder().id(mid).build();
+
+        // 리뷰 먼저 삭제되어야 한다.
+        reviewRepository.deleteByMember(member);
+        memberRepository.deleteById(mid);
+
     }
 }
