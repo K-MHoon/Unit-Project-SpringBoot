eabe2087a238ba86b22218d10fd1c856de9c9a75
K-MHOON <azmria117@gmail.com>
2022-06-09 20:27:50 +0900

    feat[Ex6] : 간단한 Security 기본세팅 추가

--- ex06/src/main/java/com/example/ex06/config/SecurityConfig.java
+++ ex06/src/main/java/com/example/ex06/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -24,5 +25,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication().withUser("user1")
                 .password("$2a$10$oOOjRESwzOVIcEdCAIV5L.39q3FPGyCcxZF0DKEHumqGtXP3Q3y4.")
                 .roles("USER");
+    }
+
+
+    // 접근제한 처리
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/sample/all").permitAll()
+                .antMatchers("/sample/member").hasRole("USER");
+        http.formLogin(); // 인증/인가 절차에서 문제가 발생했을 때 로그인 페이지를 보여주도록 지정할 수 있고, 화면으로 로그인 방식을 지원한다는 의미로 사용된다.
+        http.csrf().disable();
+        http.logout();
     }
 }
