bf8eb01326ca754d4e475141ef7f468b3097b2fb
K-MHOON <azmria117@gmail.com>
2022-06-12 10:42:41 +0900

    feat[Ex6] : rememberMe 처리, Controller @PreAuthorize 적용

--- ex06/src/main/java/com/example/ex06/config/SecurityConfig.java
+++ ex06/src/main/java/com/example/ex06/config/SecurityConfig.java
@@ -1,10 +1,13 @@
 package com.example.ex06.config;
 
 import com.example.ex06.security.handler.ClubLoginSuccessHandler;
+import com.example.ex06.security.service.ClubUserDetailsService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,8 +15,13 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @Slf4j
+@RequiredArgsConstructor
+// securedEnabled = 예전 버전의 @Secure 어노테이션 사용 가능 여부
+// prePostEnable = @PreAuthorize 사용 가능 여부
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final ClubUserDetailsService userDetailsService;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -32,13 +40,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     // 접근제한 처리
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/sample/all").permitAll()
-                .antMatchers("/sample/member").hasRole("USER");
+//        http.authorizeRequests()
+//                .antMatchers("/sample/all").permitAll()
+//                .antMatchers("/sample/member").hasRole("USER");
         http.formLogin(); // 인증/인가 절차에서 문제가 발생했을 때 로그인 페이지를 보여주도록 지정할 수 있고, 화면으로 로그인 방식을 지원한다는 의미로 사용된다.
         http.csrf().disable();
         http.oauth2Login()
                 .successHandler(successHandler());
+        http.rememberMe().tokenValiditySeconds(60*60*24*7) // 7days
+                .userDetailsService(userDetailsService);
     }
 
     @Bean
--- ex06/src/main/java/com/example/ex06/controller/SampleController.java
+++ ex06/src/main/java/com/example/ex06/controller/SampleController.java
@@ -2,6 +2,7 @@ package com.example.ex06.controller;
 
 import com.example.ex06.security.dto.ClubAuthMemberDTO;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class SampleController {
 
     // 로그인을 하지 않은 사용자도 접근 가능
+    @PreAuthorize("permitAll()")
     @GetMapping("/all")
     public void exAll() {
         log.info("exAll");
@@ -20,6 +22,7 @@ public class SampleController {
 
     // 로그인한 사용자만 접근 가능
     // @AuthenticationPrincipal은 별도의 캐스팅 작업 없이 실제 ClubAuthMemberDTO를 사용할 수 있다.
+    @PreAuthorize("hasRole('USER')")
     @GetMapping("/member")
     public void exMember(@AuthenticationPrincipal ClubAuthMemberDTO clubAuthMemberDTO) {
         log.info("exMember");
@@ -28,7 +31,18 @@ public class SampleController {
         log.info("user = {}", clubAuthMemberDTO);
     }
 
+    // @PreAuthorize의 value 표현식은 #과 같은 특별한 기호나 authentication 같은 내장 변수를 이용할 수 있다.
+    @PreAuthorize("#clubAuthMember != null && #clubAuthMember.username eq \"user95@kmhoon.com\"")
+    @GetMapping("/exOnly")
+    public String exMemberOnly(@AuthenticationPrincipal ClubAuthMemberDTO clubAuthMember) {
+        log.info("exMemberOnly");
+        log.info("{}", clubAuthMember);
+        return "/sample/admin";
+    }
+
+
     // 관리자(admin)권한이 있는 사용자만 접근 가능
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/admin")
     public void exAdmin() {
         log.info("exAdmin");
